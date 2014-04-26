ca5096675da88f127cb41d0f602615e5ea33c467
RationalAsh <ashwinnarayan1994@gmail.com>
2014-04-26 06:47:21 +0530

    Cleaned up js for new project

--- scripts/script_main.js
+++ scripts/script_main.js
@@ -1,4 +1,4 @@
-//Js file for the game of life web app.
+//Js file for the block games web app.
 //Created by rationalAsh :D
 //Now live on github.io
 
@@ -10,8 +10,21 @@ var posDisp = document.getElementById('mPos');
 var mpFlag = 0;
 var prevTBase = 0;
 
+var wHeight = screen.availHeight;
+var wWidth  = screen.availWidth;
 
+var i=0;
+var j=0;
 
+var rows = Math.round(wHeight/cellSize);
+var cols = Math.round(wWidth/cellSize);
+var gridLinesY = new Array();
+var gridLinesX = new Array();
+var cells = new Array();
+var cTemp = new Array();
+
+var snakeState = [];
+console.log(snakeState);
 //cont.onmousemove = function(e){
 
 //if((e.pageX > 1500) && (e.pageY < 410))
@@ -33,158 +46,115 @@ sBox.style.opacity = 0.8;
 sBox.style.zIndex = 1;
 sBox.style.visibility = "visible";
 
-var wHeight = screen.availHeight;
-var wWidth  = screen.availWidth;
-
-var i=0;
-var j=0;
-
-var rows = Math.round(wHeight/cellSize);
-var cols = wWidth/cellSize;
-
-//alert('rows: '+rows+' cols: '+cols);
-
-var gridLinesY = new Array();
-var gridLinesX = new Array();
-var cells = new Array();
-var cTemp = new Array();
 
 cont.onclick = function(e){
     alert("Click at: "+ e.pageX + "," + e.pageY);
 }
 
 var stage = new Kinetic.Stage({
-        container: 'container',
-        width: wWidth,
-        height: wHeight
-      });
+  container: 'container',
+  width: wWidth,
+  height: wHeight
+});
 
 var animLayer = new Kinetic.Layer();
 var statLayer = new Kinetic.Layer();
 
 var frameRateGraph = new Kinetic.Line(
-    {
-	points: [0,0],
-	stroke: "red",
-	tension: 0,
-	strokeWidth: 2
-    }
+  {
+    points: [0,0],
+    stroke: "red",
+    tension: 0,
+    strokeWidth: 2
+  }
 );
 
 //Loop to make Y gridlines
 for(i=0; i<rows; i++)
 {
-    gridLinesY.push(
-	new Kinetic.Line(
-		{
-		    points: [0, i*cellSize, wWidth, i*cellSize],
+  gridLinesY.push(
+    new Kinetic.Line(
+      {
+        points: [0, i*cellSize, wWidth, i*cellSize],
 		    stroke: 'gray',
 		    strokeWidth: 0.8,
 		    lineCap: 'round',
 		    lineJoin: 'round'
-		}
-	)
+      }
     )
+  )
 }
 //Loop to make X gridlines
 for(j=0; j<cols; j++)
 {
-    gridLinesX.push(
-	new Kinetic.Line(
-		{
-		    points: [j*cellSize, 0, j*cellSize, wHeight],
-		    stroke: 'gray',
+  gridLinesX.push(
+    new Kinetic.Line(
+      {
+        points: [j*cellSize, 0, j*cellSize, wHeight],
+        stroke: 'gray',
 		    strokeWidth: 0.8,
-		    lineCap: 'round',
-		    lineJoin: 'round'
-		}
-	)
+        lineCap: 'round',
+        lineJoin: 'round'
+      }
     )
+  )
 }
 
-//Adding Y lines to canvas
+//Adding Y lines to static layer so it doesn't have to update
 for(i=0; i<rows; i++)
 {
-    statLayer.add(gridLinesY[i]);
+  statLayer.add(gridLinesY[i]);
 }
 
-//Adding X lines to canvas
+//Adding X lines to static layer so that it doesn't have to update
 for(j=0; j<cols; j++)
 {
-    statLayer.add(gridLinesX[j]);
+  statLayer.add(gridLinesX[j]);
 }
 
 //Making the cells
 for(i=0; i<rows; i++)
 {
-    cTemp = new Array();
-    for(j=0; j<cols; j++)
-    {
-	cTemp.push(
-	    new Kinetic.Rect(
-		{
-		    x: j*cellSize + 1,
-		    y: i*cellSize + 1,
-		    width: cellSize - 2,
-		    height: cellSize - 2,
-		    fill: 'green',
-		    stroke: 'black',
-		    strokeWidth: 0
-                }
-	    )
-        )
-    }
-    cells.push(cTemp);
+  cTemp = new Array();
+  for(j=0; j<cols; j++)
+  {
+    cTemp.push(
+      new Kinetic.Rect(
+        {
+          x: j*cellSize + 1,
+          y: i*cellSize + 1,
+          width: cellSize - 2,
+          height: cellSize - 2,
+          fill: 'green',
+          stroke: 'black',
+          strokeWidth: 0
+        }
+      )
+    )
+  }
+  cells.push(cTemp);
 }
 
-//for(i=0; i<rows; i++)
-//{
-//    for(j=0; j<cols; j++)
-//    {
-//	layer.add(cells[i][j]);
-//    }
-//}
 
-animLayer.add(cells[0][0]);
-animLayer.add(cells[0][1]);
-animLayer.add(frameRateGraph);
-cells[0][0].remove();
 animLayer.draw();
 statLayer.draw();
 
 // add the layer to the stage
 stage.add(animLayer).add(statLayer);
 
+
+
+//This is the code where all the stuff that updates
+//in the next animation frame happens.
 var anim = new Kinetic.Animation(function(frame){
-    var time = frame.time,
-        timeDIff = frame.timeDiff,
-        frameRate = frame.frameRate;
-    var graphPoints = frameRateGraph.getPoints();
-    var xTime = Math.floor(time*0.3);
-    //update stuff
+  var time = frame.time,
+      timeDIff = frame.timeDiff,
+      frameRate = frame.frameRate;
+  var graphPoints = frameRateGraph.getPoints();
+  var xTime = Math.floor(time*0.3);
+  //update stuff
 
-    //cells[0][1].setX(time*0.05);
-    //cells[0][1].setY(0);
-    var timeBase = Math.floor((xTime)/(wWidth/3));
-    var X = (xTime) - (wWidth/3)*timeBase;
 
-    if(prevTBase < timeBase)
-    {
-	graphPoints = [0,wHeight/3];
-	mpFlag^=1;
-    }
-
-    graphPoints.push(X, ((wHeight/3)+5*frameRate));
-    if(xTime > wWidth/3)
-    {
-	//Need to do it twice because coordinates have 2 points
-	//graphPoints.shift();
-	//graphPoints.shift();
-    }
-    frameRateGraph.setPoints(graphPoints);
-    mPos.innerHTML = "timeBase " + mpFlag;
-
-    prevTBase = timeBase;
 
 }, animLayer);
 
